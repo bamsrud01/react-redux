d740b93d64bc9d6c21840e78f7c80832738539d6
Barrett Amsrud <barrettamsrud@gmail.com>
2017-07-04 14:00:45 -0500

    Initialized component state

--- 01-ReduxSimpleStarter/src/components/search_bar.js
+++ 01-ReduxSimpleStarter/src/components/search_bar.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 
 //  Create component
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
 
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return <input onChange={event => this.setState({ term: event.target.value })} />;
   }
 }
 

c7d05aea64bf2b765980c3e5206ff3100544bf9d
Barrett Amsrud <barrettamsrud@gmail.com>
2017-07-03 22:21:46 -0500

    Searchbar component

---
+++ 01-ReduxSimpleStarter/src/components/search_bar.js
@@ -0,0 +1,10 @@
+//  Import libraries
+import React from 'react';
+
+//  Create component
+const SearchBar = () => {
+  return <input />;
+};
+
+//  Export component
+export default SearchBar;
--- 01-ReduxSimpleStarter/src/index.js
+++ 01-ReduxSimpleStarter/src/index.js
@@ -2,12 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//  Import components
+import SearchBar from './components/search_bar';
+
 //  Declare API key
 const API_KEY = 'AIzaSyCVW_HM7t8Tfadc8Tb9JAfrEkNfjn5PboY';
 
 //  Create component
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 //  Render element

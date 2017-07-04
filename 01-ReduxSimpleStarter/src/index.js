428669c9aa63fc19a86b58b7d4deda2830ed2d2d
Barrett Amsrud <barrettamsrud@gmail.com>
2017-07-03 21:48:04 -0500

    Added youtube API

--- 01-ReduxSimpleStarter/src/index.js
+++ 01-ReduxSimpleStarter/src/index.js
@@ -2,8 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//  Declare API key
+const API_KEY = 'AIzaSyCVW_HM7t8Tfadc8Tb9JAfrEkNfjn5PboY';
+
 //  Create component
-const App = function() {
+const App = () => {
   return <div>Hi!</div>;
 };
 

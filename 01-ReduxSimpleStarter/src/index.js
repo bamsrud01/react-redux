230fd46bc7c43b4c07cd27dc2166e309b9f338b4
Barrett Amsrud <barrettamsrud@gmail.com>
2017-07-05 20:53:39 -0500

    Called youtube api search

--- 01-ReduxSimpleStarter/src/index.js
+++ 01-ReduxSimpleStarter/src/index.js
@@ -1,12 +1,17 @@
 //  Import libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 //  Import components
 import SearchBar from './components/search_bar';
 
 //  Declare API key
 const API_KEY = 'AIzaSyCVW_HM7t8Tfadc8Tb9JAfrEkNfjn5PboY';
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+  console.log(data);
+});
 
 //  Create component
 const App = () => {

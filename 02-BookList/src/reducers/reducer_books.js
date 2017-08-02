1ce973b2e8733124a7c3bf16fc6c587bee0c255e
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-01 22:20:28 -0500

    Began writing reducers

---
+++ 02-BookList/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+//  Create and export books reducer
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts' },
+    { title: 'Harry Potter and the Prisoner of Azkaban' },
+    { title: 'The Dark Tower' },
+    { title: 'A Game of Thrones' }
+  ];
+}

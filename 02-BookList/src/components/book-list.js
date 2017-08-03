5354c960b854b3c30306a306dd569ebf568a9eb7
Barrett Amsrud <barrettamsrud@gmail.com>
2017-08-02 23:15:29 -0500

    Created BookList component

---
+++ 02-BookList/src/components/book-list.js
@@ -0,0 +1,23 @@
+//  Import libraries
+import React, { Component } from 'react';
+
+//  Create class
+export default class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group-item">
+          {book.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
